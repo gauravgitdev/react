32e70f57260003650a1df45f3e0ae3d13098d04a
Gaurav Kumar <gauravkumar086421@gmail.com>
2025-08-08 11:56:39 -0700

    zod valin

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
   const cartItems = useSelector((store)=>store.cart.cartItems)
   return (
     
-    
+  
     <div className='navbar'>
       <h1>Logo</h1>
       <ul className='menu-items'>
